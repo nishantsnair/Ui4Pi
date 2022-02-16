95deccc873abfd304f63463ba061b5b57f206548
nishantsnair <nishant2006in@gmail.com>
2022-02-16 20:17:52 +0100

    added one more tab

--- ui.py
+++ ui.py
@@ -3,14 +3,14 @@ import pygubu
 import tkinter as tk
 import tkinter.ttk as ttk
 
-PROJECT_PATH = pathlib.Path(__file__).parent
-PROJECT_UI = PROJECT_PATH / "newproject"
 
 PADX = 30
 PADY = 20
 class NewprojectApp:
     def __init__(self, master=None):
         # build ui
+        self.nb = ttk.Notebook(root)
+        self.nb.pack()
         self.mainFrame = ttk.Frame(master)
         self.button1 = ttk.Button(self.mainFrame)
         self.img_lightbulbiconOn = tk.PhotoImage(file='light-bulb-icon.png')
@@ -32,42 +32,65 @@ class NewprojectApp:
         self.switch3Text = tk.StringVar(value='switch3 (OFF)')
         self.button3.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
         self.button3.configure(text='switch3', textvariable=self.switch3Text)
-        self.button3.grid(column='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='0')
+        self.button3.grid(column='2', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='0')
         self.button3.configure(command=self.toggleSwitch3)
         self.button3State = False
         self.button4 = ttk.Button(self.mainFrame)
         self.switch4Text = tk.StringVar(value='switch4 (OFF)')
         self.button4.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
         self.button4.configure(text='switch4', textvariable=self.switch4Text)
-        self.button4.grid(column='4', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='0')
+        self.button4.grid(column='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='0')
         self.button4.configure(command=self.toggleSwitch4)
         self.button4State = False
         self.button5 = ttk.Button(self.mainFrame)
         self.switch5Text = tk.StringVar(value='switch5 (OFF)')
         self.button5.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
         self.button5.configure(text='switch5', textvariable=self.switch5Text)
-        self.button5.grid(column='0', columnspan='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
+        self.button5.grid(column='0', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
         self.button5.configure(command=self.toggleSwitch5)
         self.button5State = False
         self.button6 = ttk.Button(self.mainFrame)
         self.switch6Text = tk.StringVar(value='switch6 (OFF)')
         self.button6.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
         self.button6.configure(text='switch6', textvariable=self.switch6Text)
-        self.button6.grid(column='1', columnspan='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
+        self.button6.grid(column='1', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
         self.button6.configure(command=self.toggleSwitch6)
         self.button6State = False
         self.button7 = ttk.Button(self.mainFrame)
         self.switch7Text = tk.StringVar(value='switch7 (OFF)')
         self.button7.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
         self.button7.configure(text='switch7', textvariable=self.switch7Text)
-        self.button7.grid(column='3', columnspan='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
+        self.button7.grid(column='2', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
         self.button7.configure(command=self.toggleSwitch7)
         self.button7State = False
+        self.button8 = ttk.Button(self.mainFrame)
+        self.switch8Text = tk.StringVar(value='switch8 (OFF)')
+        self.button8.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')
+        self.button8.configure(text='switch8', textvariable=self.switch8Text)
+        self.button8.grid(column='3', ipadx='0', ipady='0', padx=PADX, pady=PADY, row='1')
+        self.button8.configure(command=self.toggleSwitch8)
+        self.button8State = False
         self.mainFrame.configure(cursor='pirate', height='600', width='800')
-        self.mainFrame.grid(column='0', row='0')
+        # self.mainFrame.grid(column='0', row='0')
+        self.nb.add(self.mainFrame,text="main")
 
+        # second page
+        self.options = ttk.Frame(master)
+        self.update = ttk.Button(self.options)
+        self.img_update = tk.PhotoImage(file='update.png')
+        self.update.configure(compound='top', image=self.img_update, text='Update')
+        self.update.grid(column='2', padx='220', pady='40', row='0')
+        self.update.configure(command=self.runUpdate)
+        self.exit = ttk.Button(self.options)
+        self.img_exit = tk.PhotoImage(file='exit.png')
+        self.exit.configure(compound='top', cursor='arrow', image=self.img_exit, state='normal')
+        self.exit.configure(text='Exit')
+        self.exit.grid(column='2', padx='220', pady='40', row='1')
+        self.exit.configure(command=self.closeUI)
+        self.options.configure(height='200', width='200')
+        self.nb.add(self.options,text="options")     
         # Main widget
-        self.mainwindow = self.mainFrame
+        self.mainwindow = self.nb
     
     def run(self):
         self.mainwindow.mainloop()
@@ -170,9 +193,42 @@ class NewprojectApp:
             print("turned on 7")
             self.button7State = True
 
+    def toggleSwitch8(self):
+        if self.button8State:
+            self.button8.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOff, state='normal')    
+            self.switch8Text = tk.StringVar(value='switch8 (OFF)')
+            self.button8.configure(text='switch8', textvariable=self.switch8Text)    
+            print("turned off 8")
+            self.button8State = False
+        else:
+            self.button8.configure(compound='top', cursor='sailboat', image=self.img_lightbulbiconOn, state='normal')    
+            self.switch8Text = tk.StringVar(value='switch8 (ON)')
+            self.button8.configure(text='switch8', textvariable=self.switch8Text)    
+            print("turned on 8")
+            self.button8State = True
+    
+    def runUpdate(self):
+        import os
+        import subprocess
+        import time
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(dir_path)
+        updateProcess = subprocess.Popen("git pull", shell=True)
+        # while updateProcess.poll() is None: time.sleep(0.1)
+        self.img_updated = tk.PhotoImage(file='updated.png')
+        self.update.configure(compound='top', image=self.img_updated, text='Updated!')
+
+    def closeUI(self):
+        root.destroy()
 
 if __name__ == '__main__':
     root = tk.Tk()
+    style = ttk.Style()
+    style.theme_create( "MyStyle", parent="vista", settings={
+        # "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0] } },
+        "TNotebook.Tab": {"configure": {"padding": [120, 10] },}})
+
+    style.theme_use("MyStyle")
     app = NewprojectApp(root)
     app.run()
 
